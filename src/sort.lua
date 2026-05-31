155a2add5e49cf4381adf7c1b5c965f8da19302a
Alexandre Barreto <121456361+IfLostart2per5@users.noreply.github.com>
2026-05-31 16:42:12 -0300

    dev(refactor): simplified sort function
    I didn't know about `table.sort` until yesterday, so I've changed it from a manual insertion sort to native table.sort, with a decreasing order.

--- src/sort.lua
+++ src/sort.lua
@@ -1,15 +1,7 @@
 local function sort(list, criterion)
-    for i = 2, #list do
-        local key = list[i]
-        local j = i - 1
-
-        while j >= 1 and criterion(list[j]) > criterion(key) do
-            list[j + 1] = list[j]
-            j = j - 1
-        end
-
-        list[j + 1] = key
-    end
+    return table.sort(list, function(a, b)
+        return criterion(a) > criterion(b)
+    end)
 end
 
 return sort
